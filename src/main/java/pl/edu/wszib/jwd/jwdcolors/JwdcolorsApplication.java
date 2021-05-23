84f4c39937d8b241f6df76e104988645c6b2b47a
Anna Adamik <quenthel5@gmail.com>
2021-05-23 09:19:01 +0200

    konfiguracja bazy danych

--- src/main/java/pl/edu/wszib/jwd/jwdcolors/JwdcolorsApplication.java
+++ src/main/java/pl/edu/wszib/jwd/jwdcolors/JwdcolorsApplication.java
@@ -2,11 +2,14 @@ package pl.edu.wszib.jwd.jwdcolors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:db.properties")
 public class JwdcolorsApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(JwdcolorsApplication.class, args);
     }
 
